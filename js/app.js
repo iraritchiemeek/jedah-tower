f0acd94bfa992bf4aa4b2b4d465b42f8c5aed350
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-09-18 13:08:02 +0900

    everything

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
-var hakApp = angular.module('hakApp', []);
+var iraApp = angular.module('iraApp', []);
 
-hakApp.controller('mainController', function($scope) {
-	$scope.index = 0;
-	$scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
-
-	$scope.decrementIndex = function(array) {
-	    if ($scope.index <= 0) {
-	        $scope.index = array.length -1
-	    } else {
-	        $scope.index -= 1
-	    }
-	}
-
-	$scope.incrementIndex = function(array) {
-	    if ($scope.index >= array.length -1) {
-	        $scope.index = 0
-	    } else {
-	        $scope.index += 1
-	    }
-	}
-
+iraApp.controller('mainController', function($scope) {
+	
 })
