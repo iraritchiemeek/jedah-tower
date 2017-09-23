660177899249d81f044aedc096d5a8252e369762
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-09-23 19:55:58 +0900

    new images and add strips

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 var iraApp = angular.module('iraApp', []);
 
 iraApp.controller('mainController', function($scope) {
-	
+
+	var init = function () {
+		$scope.setImageStripWidth()
+	}
+
+	$scope.setImageStripWidth = function () {
+		$('.image-strip').css({'width': (100/$('.image-strip').length)+'%'})
+	}
+
+	init()
 })
