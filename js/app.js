48cfcd4e29b40652383211c35641c41e983e363a
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-10-16 13:15:27 +0900

    horizontal image strips if mobile

--- js/app.js
+++ js/app.js
@@ -3,16 +3,23 @@ var iraApp = angular.module('iraApp', []);
 iraApp.controller('mainController', ["$scope", function($scope) {
 
 	var init = function () {
-		$scope.randomImageJumbleCss()
+		// $scope.randomImageJumbleCss()
+		$(".mobile_test").css("display") == "none" ? $scope.setImageStripHeight() : $scope.setImageStripWidth()
 	}
 
-	$scope.scrollTo = function (e) {
-		e == 'top' ? element = '.image-jumble-container' : element = '#set-' + e.target.id	
-		$(element)[0].scrollIntoView({block: 'center', behavior: 'smooth'})
+	$scope.scrollTo = function (id) {
+		id == 'top' ? element = '.image-jumble-container' : element = '#set-' + id	
+		$(element)[0].scrollIntoView({block: 'center', behavior: 'instant'})
 	}
 
 	$scope.setImageStripWidth = function () {
+		$('.image-strip').css({'height': '100%'})
 		$('.image-strip').css({'width': (100/$('.image-strip').length)+'%'})
+	}
+
+	$scope.setImageStripHeight = function () {
+		$('.image-strip').css({'width': '100%'})
+		$('.image-strip').css({'height': (100/$('.image-strip').length)+'%'})
 	}
 
 	$scope.randomImageJumbleCss = function () {
@@ -45,6 +52,29 @@ iraApp.controller('mainController', ["$scope", function($scope) {
 
 	init()
 }])
+
+$(document).ready(function () {
+	
+	$(window).resize(function () {
+		responsive()
+	})
+
+	var responsive = function() {
+		$(".mobile_test").css("display") == "none" ? setImageStripHeight() : setImageStripWidth()
+	}
+
+	var setImageStripWidth = function () {
+		$('.image-strip').css({'height': '100%'})
+		$('.image-strip').css({'width': (100/$('.image-strip').length)+'%'})
+	}
+
+	var setImageStripHeight = function () {
+		$('.image-strip').css({'width': '100%'})
+		$('.image-strip').css({'height': (100/$('.image-strip').length)+'%'})
+	}
+
+})
+
 
 $(window).on("load", function() {
 	$('.loading').hide()
